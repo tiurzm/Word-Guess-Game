30f409c001d92b023063d1c6243cb293d4caee1d
Tiur Mawaddah <tiurzm@gmail.com>
2019-09-22 08:10:54 -0500

    <COMMENT>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,17 @@
-// creat an array of the songs
+// create an array of the songs and choose a song randomly
 var animeSongs = ["dragonball", "inuyasha", "pokemon", "hunterxhunter", "sailormoon", "onepiece", "gundam", "digimon"];
-// choose a song randomly
+
 var aSong = animeSongs[Math.floor(Math.random() * animeSongs.length)];
-console.log(aSong);
+
 // create underscores based on the length of songs
 var underScore = [];
 for(var i = 0; i < aSong.length; i++){
-    // underScore[i] = "_"; 
     underScore.push("_");
 }; 
-console.log(underScore);
+
 // display underscores to the screen
 var currentWord = document.getElementById("word");
 currentWord.textContent = underScore.join(" ");
-
-// function allGame() { 
-//     game();
-//     if(){
-//         winScore++;
-//         userWins.textContent = winScore; 
-//     };  
-// };
 
 // GAME
 var letterGuess = document.getElementById("letters");
@@ -30,24 +21,55 @@ var alLetter = [];
 var numGuess = 12;
 var winScore = 0;
 
+
 // get user guess
 document.onkeyup = game; 
 function game() {
-var userPress = event.key;
+var userPress = event.key.toLowerCase();
     if (aSong.indexOf(userPress) > -1) {
         for(var i = 0; i < aSong.length; i++) {
            if(aSong[i] === userPress){
             underScore[i] = userPress;
             currentWord.textContent = underScore.join(" ");
-            console.log(underScore);
-           }  
-        }
+           };
+        };     
+        
     } else {
-        alLetter.push(userPress);
-        --numGuess;
-        remainNum.textContent = numGuess;
-        letterGuess.textContent = alLetter + ",";
-        console.log(alLetter);
-        console.log(numGuess);
-    }
+        if (alLetter.indexOf(userPress) === -1) {
+            alLetter.push(userPress);
+            numGuess--;
+            remainNum.textContent = numGuess;
+            letterGuess.textContent = alLetter + ",";
+            console.log(alLetter);
+            console.log(numGuess);
+        };
+    };
+
+if(!underScore.includes("_")){
+        winScore++;
+        userWins.textContent = winScore;
+        reset();
+    };
+
+
+if(numGuess === 0) {
+    alert("you lose");
+    return reset();
+    };
 }; 
+
+
+
+// reset game
+function reset(){
+    numGuess = 12 ;
+    remainNum.textContent = 12; 
+    aSong = animeSongs[Math.floor(Math.random() * animeSongs.length)];
+    underScore = [];
+    for(var i = 0; i < aSong.length; i++){
+        underScore.push("_");
+    }; 
+    currentWord.textContent = underScore.join(" ");
+    alLetter = [];
+    letterGuess.textContent = "";
+};
